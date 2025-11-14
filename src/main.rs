3dd4c6b3c562fa3f7868945fac94c6a4f1b0ef2d
Qwerier <klarkahmeti@yahoo.com>
2025-11-14 16:11:40 +0100

    Implement error handling and refactor logic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
-use std::{env, fs};
+pub mod lib;
+
+use std::{env, error::Error, fs, process};
 
 fn main() {
     println!("Hello, world!");
     let args:Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args)
+        .unwrap_or_else(|err|{
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        });
 
-    println!("In file {file_path}");
+    println!("Searching for '{}'", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should be able to read the file");
+    match run(config) {
+      Err(err)=> {
+        println!("Application error: {err}");
+        process::exit(1);
+      },
+      _ => {},
+    };
+
+    dbg!(args);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("File contains:\n{contents}");
 
-    dbg!(args);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() <3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
---
+++ target/debug/deps/repgrep-689ea80b6d60769d.d
@@ -0,0 +1,6 @@
+/home/perdorues/Desktop/rust/repgrep/target/debug/deps/librepgrep-689ea80b6d60769d.rmeta: src/main.rs src/lib.rs
+
+/home/perdorues/Desktop/rust/repgrep/target/debug/deps/repgrep-689ea80b6d60769d.d: src/main.rs src/lib.rs
+
+src/main.rs:
+src/lib.rs:
