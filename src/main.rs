a1a63640febc2596418a4d8f6224976d41a2507e
Qwerier <klarkahmeti@yahoo.com>
2025-11-14 17:33:16 +0100

    Add case insensitive search

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let config = Config::build(&args)
         .unwrap_or_else(|err|{
-            println!("Problem parsing arguments: {err}");
+            eprintln!("Problem parsing arguments: {err}");
             process::exit(1);
         });
 
@@ -17,11 +17,9 @@ fn main() {
 
     match repgrep::run(config) {
       Err(err)=> {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
       },
       _ => {},
     };
-
-    dbg!(args);
 }
